7bd56ef50bdfe5503556f3a00f156c341b8b8864
Ronny Haase <ronhaase@gmail.com>
2013-04-08 17:16:06 +0200

    Fine tuning touch...

--- swipo.js
+++ swipo.js
@@ -12,33 +12,62 @@ if ( !(Hammer) ) {
 
 "use strict";
 
+var
+	isDragging = false
+	, dragInfo = {
+		startX: null
+	}
+
 //
 // Data API
 //
-/*$('[data-toggle="swipo-deck-right"]').each(function() {
-	Hammer(this).on('swiperight', function() {
-		$(this).parent().removeClass('right');
-	})
-})
-
-$('[data-toggle="swipo-deck-left"]').each(function() {
-	Hammer(this).on('swipeleft', function() {
-		$(this).parent().removeClass('left');
-	})
-})*/
-
 $('[data-toggle="swipo-deck"]').each(function() {
+	var $this = $(this)
+		, $sdc = $this.find('[data-toggle="swipo-deck-center"]')
+
 	Hammer(this).on('swipeleft', function() {
-		if ( $(this).hasClass('left') )
-			$(this).removeClass('left')
+		if ( $this.hasClass('left') )
+			$this.removeClass('left')
 		else if ( !$(this).hasClass('right') )
-			$(this).addClass('right')
+			$this.addClass('right')
 	})
 	.on('swiperight', function() {
-		if ( $(this).hasClass('right') )
-			$(this).removeClass('right')
-		else if ( !$(this).hasClass('left') )
-			$(this).addClass('left')
+		if ( $this.hasClass('right') )
+			$this.removeClass('right')
+		else if ( !$this.hasClass('left') )
+			$this.addClass('left')
+	})
+
+	$this.on('mousedown', function(ev) {
+		var $this = $(this)
+
+		isDragging = true
+		dragInfo.startX = ev.pageX
+		dragInfo.startY = ev.pageY
+		console.log( $this.parentsUntil('.swipo').parent().addClass('no-trans') )
+	})
+
+	$this.on('mouseup', function(ev) {
+		var $this = $(this)
+
+		isDragging = false
+		dragInfo.startX = null
+		dragInfo.startY = null
+		console.log( $this.parentsUntil('.swipo').parent().removeClass('no-trans') )
+
+		$sdc.css('left','')
+	})
+
+	$this.on('mousemove', function(ev) {
+		var $this = $(this)
+
+		if ( !isDragging )
+			return
+
+		if ( $sdc.length === 0 )
+			return
+
+		$sdc.css('left', ev.pageX + 'px')
 	})
 })
 
